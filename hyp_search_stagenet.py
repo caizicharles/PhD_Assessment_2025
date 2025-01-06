2941e4474924dba0d1079bb0599218b45c33d25d
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2025-01-06 19:24:48 +0000

    Update hyp_search_stagenet.py

--- hyp_search_stagenet.py
+++ hyp_search_stagenet.py
@@ -10,5 +10,5 @@ for file in config_files:
         for level in levels:
             for lr in lr_vals:
                 os.system(f'python main.py \
-                        -c C:/Users/ZC/Documents/GitHub/PhD_Assessment_2025/configs/{file}.yaml \
+                        -c {PATH_TO_REPO}/PhD_Assessment_2025/configs/{file}.yaml \
                             --seed 0 --save_test {True} --lr {lr} --conv_size {size} --levels {level}')
