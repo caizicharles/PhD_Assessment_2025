caee0216dd489d9d4d08821e52f210fdaa6f2579
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2025-01-06 19:24:18 +0000

    Update hyp_search_base.py

--- hyp_search_base.py
+++ hyp_search_base.py
@@ -20,7 +20,7 @@ for file in config_files:
                         for fn in act_fn:
                             for lr in lr_vals:
                                 os.system(f'python main.py \
-                                        -c C:/Users/ZC/Documents/GitHub/PhD_Assessment_2025/configs/{file}.yaml \
+                                        -c {PATH_TO_REPO}/PhD_Assessment_2025/configs/{file}.yaml \
                                             --seed 0 \
                                             --save_test {True} \
                                             --lr {lr} \
