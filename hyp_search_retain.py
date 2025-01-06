5962dc62400104cc68503c5ce76d5c5a5440e98e
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2025-01-06 19:24:39 +0000

    Update hyp_search_retain.py

--- hyp_search_retain.py
+++ hyp_search_retain.py
@@ -6,5 +6,5 @@ lr_vals = [0.0001, 0.0005, 0.001]
 for file in config_files:
     for lr in lr_vals:
         os.system(f'python main.py \
-                -c C:/Users/ZC/Documents/GitHub/PhD_Assessment_2025/configs/{file}.yaml \
+                -c {PATH_TO_REPO}/PhD_Assessment_2025/configs/{file}.yaml \
                     --seed 0 --save_test {True} --lr {lr}')
