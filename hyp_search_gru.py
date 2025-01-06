9ffee01c0492446bceaeeeb80b5b6d217a1f07f7
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2025-01-06 19:24:29 +0000

    Update hyp_search_gru.py

--- hyp_search_gru.py
+++ hyp_search_gru.py
@@ -8,5 +8,5 @@ for file in config_files:
     for layer in layer_vals:
         for lr in lr_vals:
             os.system(f'python main.py \
-                    -c C:/Users/ZC/Documents/GitHub/PhD_Assessment_2025/configs/{file}.yaml \
+                    -c {PATH_TO_REPO}/PhD_Assessment_2025/configs/{file}.yaml \
                         --seed 0 --save_test {True} --lr {lr} --layer_num {layer}')
